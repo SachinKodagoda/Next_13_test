0234d58baeeb47f9a885e87d5bca2ad6126acf9d
Duminda Kodagoda <duminda.g.k@gmail.com>
2023-01-01 18:01:30 +0530

    ctx added

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { HiBars3BottomRight, HiOutlineMoon, HiOutlineSun } from 'react-icons/hi2';
+import CartIcon from './CartIcon';
+
+import styles from './navbar.module.css';
+
+function Navbar(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <HiOutlineSun size='50px' color='white' />
+      <HiOutlineMoon size='50px' color='white' />
+      <HiBars3BottomRight size='50px' color='white' />
+      <CartIcon />
+    </div>
+  );
+}
+
+export default Navbar;
